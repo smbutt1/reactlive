280131665fec893f0f0654bb082c91b88403ecc1
smbutt1 <98937511+smbutt1@users.noreply.github.com>
2022-09-11 12:18:33 +0500

    React Assignment No. 2

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,66 +1,87 @@
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
-import React from 'react';
- 
-class Header extends Component {
-    constructor(props) {
-        super(props);
+import React, { Component } from 'react';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from "reactstrap";
+import {  NavLink } from 'react-router-dom';
+
+class Header extends Component{
     
-        this.toggleNav = this.toggleNav.bind(this);
-        this.state = {
-          isNavOpen: false
+    constructor(props){
+        super(props);
+
+        this.state={
+            isNavOpen: false
         };
-        this.toggleNav = this,this.toggleNav.bind(this);
-      }
- 
-      toggleNav() {
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav(){
         this.setState({
-          isNavOpen: !this.state.isNavOpen
+            isNavOpen: !this.state.isNavOpen
         });
-      }
- 
-    render() {
-return(
-    <React.Fragment>
-<Navbar dark expand="md">
+    }
+
+    render(){
+        return(
+            // react fragment
+            // <>
+            // </>
+            <React.Fragment>
+                <Navbar dark expand="md">                   
                     <div className="container">
+
                         <NavbarToggler onClick={this.toggleNav} />
-                        <NavbarBrand className="mr-auto" href="/"><img src='assets/images/logo.png' height="30" width="41" alt="Ristorante Con Fusion" /></NavbarBrand>
+
+                        <NavbarBrand className="mr-auto" href="/">
+                            <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" />
+                        </NavbarBrand>
+
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
-                            <NavItem>
-                                <NavLink className="nav-link"  to='/home'>
-                                    <span className="fa fa-home fa-lg"></span> Home</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to='/aboutus'>
-                                    <span className="fa fa-info fa-lg"></span> About Us</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link"  to='/menu'>
-                                    <span className="fa fa-list fa-lg"></span> Menu</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to='/contactus'>
-                                    <span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
-                            </NavItem>
-                            </Nav>
-                            </Collapse>
-                   </div>   
-                   </Navbar>      
-<Jumbotron>
-<div className="container">
-<div className="row row-header">
-<div className="col-12 col-sm-6">
-<h1>Ristorante con Fusion</h1>
-<p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
-</div>
-</div>
-</div> 
-</Jumbotron>
-</React.Fragment>
-);
-}
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                    <span className="fa fa-home fa-lg" ></span>Home
+                                    </NavLink>
+                                </NavItem>
+                                
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                    <span className="fa fa-info fa-lg" ></span>About Us
+                                    </NavLink>
+                                </NavItem>
+                                
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/menu">
+                                        <span className="fa fa-list fa-lg" ></span>Menu
+                                    </NavLink>
+                                </NavItem>
+                                
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <span className="fa fa-address-card fa-lg" ></span>Contact Us
+                                    </NavLink>
+                                </NavItem>
+
+                            </Nav>                        
+                        </Collapse>
+                    </div>
+                </Navbar>
+
+                <Jumbotron>
+                    <div className="container">
+                        <div className="row row-header">
+                            <div className="col-12 col-sm-6">
+                                <h1> Ristorante Con Fusion </h1>
+                                <p> 
+                                    We take inspiration from te World's best cuisines, and create a unique 
+                                    fusion experience. Our lipsmacking creations 
+                                    will ti ckle your culinary senses!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </Jumbotron>
+            </React.Fragment>
+        );
+    }
 }
 
 export default Header;
