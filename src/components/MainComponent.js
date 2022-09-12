d262e649003b32bb1cd8e6cda9a79195453a9ac4
smbutt1 <98937511+smbutt1@users.noreply.github.com>
2022-09-12 23:17:10 +0500

    Intro. to React Redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,43 +1,40 @@
 import React, { Component } from 'react';
-
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
+import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import DishDetail from './DishdetailComponent';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import { COMMENTS } from '../shared/comments'
-import { DISHES } from '../shared/dishes'
-import { LEADERS } from '../shared/leaders'
-import { PROMOTIONS } from '../shared/promotions'
-
-import { Switch, Route, Redirect } from 'react-router-dom';
-
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+        promotions: state.promotions,
+        leaders: state.leaders
+      };
+    }
 
 class Main extends Component {
 
     constructor(props) {
         super(props);
 
-        this.state = {
-            comments: COMMENTS,
-            dishes: DISHES,
-            leaders: LEADERS,
-            promotions: PROMOTIONS,
-        };
-
-    }
-
+         }
+         
+           
+          
     render() {
 
         const HomePage = () => {
             return(
                 <Home 
-                    dish={ this.state.dishes.filter( (dish)=>dish.featured )[0] }
-                    promotion={this.state.promotions.filter( (promotion)=>promotion.featured )[0] }
-                    leader={this.state.leaders.filter( (leader)=>leader.featured )[0] }
+                    dish={ this.props.dishes.filter( (dish)=>dish.featured )[0] }
+                    promotion={this.props.promotions.filter( (promotion)=>promotion.featured )[0] }
+                    leader={this.props.leaders.filter( (leader)=>leader.featured )[0] }
                 />
             );
         };
@@ -45,7 +42,7 @@ class Main extends Component {
         const AboutUsPage = () => {
             return(
                 <About 
-                    leaders={this.state.leaders}
+                    leaders={this.props.leaders}
                 />
             );
         };
@@ -55,8 +52,8 @@ class Main extends Component {
             return(
                 <DishDetail 
                 
-                dish={this.state.dishes.filter( (dish) => dish.id === parseInt(match.params.dishId, 10))[0] } 
-                comments={this.state.comments.filter( (comment) => comment.dishId === parseInt(match.params.dishId, 10)) } 
+                dish={this.props.dishes.filter( (dish) => dish.id === parseInt(match.params.dishId, 10))[0] } 
+                comments={this.props.comments.filter( (comment) => comment.dishId === parseInt(match.params.dishId, 10)) } 
                 
                 
                 />
@@ -71,12 +68,11 @@ class Main extends Component {
 
                 <Switch>
                     <Route path="/home" component={ HomePage } />
-                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/> }/>
-
+                    <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders}/> }/>
+                    <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes}/> }/>
                     <Route path="/menu/:dishId" component={DishWithId} />
-
                     <Route exact path="/contactus" component={Contact } />
-                    <Route exact path="/aboutus" component={ AboutUsPage } />
+                    
                    
                     {/* if url dosesnt match, bydefault redirect to */}
                     <Redirect to="/home" />
@@ -90,4 +86,4 @@ class Main extends Component {
 
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
